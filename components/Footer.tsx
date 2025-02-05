275b2be0ebba89f731bb22b154bc6543ceb86f81
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-02-05 09:13:27 -0300

    refatora componente Footer para incluir ícone de email e ajusta layout; remove redundância na descrição do componente Experience

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-import { GithubLogo, LinkedinLogo } from 'phosphor-react';
-import { Button } from './ui/button';
+import { GithubLogo, LinkedinLogo, EnvelopeSimple } from 'phosphor-react';
 
 export function Footer() {
   return (
     <footer
       id="contact"
-      className="bg-primary p-8 px-40 text-primary-foreground shadow-lg dark:bg-secondary dark:text-secondary-foreground"
+      className="bg-primary p-8 px-4 text-primary-foreground shadow-lg dark:bg-secondary dark:text-secondary-foreground md:px-40"
     >
-      <div className="container flex flex-col items-center text-left">
-        <h2 className="mb-4 mr-[720px] max-w-3xl text-left text-lg">Sobre</h2>
+      <div className="container flex flex-col items-start px-96 text-left">
+        <h2 className="mb-4 max-w-3xl text-left text-lg">Sobre</h2>
         <p className="mb-4 max-w-3xl text-left text-lg">
           Desenvolvedor de Software com mais de 4 anos de experiência na entrega
           de soluções robustas e de alta qualidade. Especialista em React, React
@@ -19,7 +18,7 @@ export function Footer() {
           TypeScript, além de ampla experiência em JavaScript, SQL e MongoDB.
         </p>
 
-        <div className="mb-4 flex space-x-6">
+        <div className="mb-4 flex flex-col items-center gap-6 sm:flex-row">
           <a
             href="https://github.com/stecks10"
             target="_blank"
@@ -28,12 +27,15 @@ export function Footer() {
             <GithubLogo size={30} className="transition hover:text-rose-400" />
           </a>
 
-          <Button
-            variant="light"
+          <a
+            href="mailto:vnn2006@gmail.com"
             className="px-6 py-2 font-bold transition-colors dark:border-rose-600"
           >
-            Enviar Email
-          </Button>
+            <EnvelopeSimple
+              size={30}
+              className="transition hover:text-rose-400"
+            />
+          </a>
           <a
             href="https://www.linkedin.com/in/vitor-nunes-do-nascimento-466004197/"
             target="_blank"
