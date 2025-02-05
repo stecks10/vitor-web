bf74c9b90082b48d1fa6319766f0225819bfeb43
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-02-05 10:22:00 -0300

    refatora estilos globais e componentes para melhorar responsividade e layout

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -20,21 +20,21 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({
   period,
   description,
 }) => (
-  <div className="flex items-start space-x-6">
+  <div className="flex flex-col items-start space-y-6 sm:flex-row sm:space-x-6 sm:space-y-0">
     <Image
       src={imgSrc}
       alt={imgAlt}
       width={48}
       height={48}
-      className="h-12 w-12 rounded-lg"
+      className="h-12 w-12 rounded-lg sm:h-16 sm:w-16"
     />
     <div className="space-y-3">
-      <h3 className="text-2xl font-bold">{title}</h3>
+      <h3 className="text-xl font-bold sm:text-2xl">{title}</h3>
       <p className="font-semibold text-gray-400">
         {company} · {period}
       </p>
       {description.map((desc, index) => (
-        <p key={index} className="text-base leading-relaxed">
+        <p key={index} className="text-sm leading-relaxed sm:text-base">
           {desc}
         </p>
       ))}
@@ -44,13 +44,13 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({
 
 export function Experience() {
   return (
-    <div className="pb-32 pt-10">
+    <div className="pb-16 pt-10 sm:pb-32">
       <section id="experience">
-        <h2 className="mb-10 text-center text-4xl font-bold text-rose-600">
+        <h2 className="mb-10 text-center text-3xl font-bold text-rose-600 sm:text-4xl">
           Experiência
         </h2>
 
-        <div className="mx-auto max-w-4xl space-y-10">
+        <div className="mx-auto max-w-2xl space-y-10 sm:max-w-4xl">
           <ExperienceItem
             imgSrc="/7comm.jpeg"
             imgAlt="7comm"
@@ -58,14 +58,14 @@ export function Experience() {
             company="7COMm - People and tech to transform"
             period="ago de 2023 - set de 2024 · 1 ano 5 meses"
             description={[
-              'Desenvolvimento de sistema de renegociação de dívidas para o Banco Bradesco utilizando Node.js no Backend for Frontend (BFF) e Angular no frontend. O backend foi estruturado com NestJS, garantindo uma arquitetura modular e escalável, permitindo que diferentes componentes do sistema fossem facilmente atualizados e mantidos. Implementação de integração com microsserviços e micro-frontend, com foco em flexibilidade e escalabilidade. Utilização de Mocha para testes unitários no BFF e Jest no frontend, além de Bamboo para integração contínua e SonarQube para análise de qualidade de código.',
-              'A gestão do projeto foi realizada através de Jira, facilitando o acompanhamento das tarefas, sprints e definição de prioridades. Utilizamos Azure para o gerenciamento de deploy e a infraestrutura de nuvem, garantindo a alta disponibilidade e escalabilidade do sistema. A automação de testes, integração contínua e práticas de DevOps foram implementadas para garantir a qualidade do código e a entrega contínua de novas funcionalidades.',
-              'Metodologias ágeis (Scrum) para gerenciar o desenvolvimento e garantir entregas rápidas e consistentes. Trabalhei de forma colaborativa com uma equipe multifuncional, incluindo desenvolvedores backend, frontend, QA e DevOps, garantindo uma comunicação eficiente e a entrega de alta qualidade. Implementação de logs de rastreabilidade para monitoramento e análise de performance, além de um acompanhamento constante de métricas para otimização do sistema.',
+              'Desenvolvimento de sistema de renegociação de dívidas para o Banco Bradesco utilizando Node.js no Backend for Frontend (BFF) e Angular no frontend...',
+              'A gestão do projeto foi realizada através de Jira, facilitando o acompanhamento das tarefas, sprints...',
+              'Metodologias ágeis (Scrum) para gerenciar o desenvolvimento e garantir entregas rápidas...',
             ]}
           />
         </div>
 
-        <div className="mx-auto mt-20 max-w-4xl space-y-10">
+        <div className="mx-auto mt-20 max-w-2xl space-y-10 sm:max-w-4xl">
           <ExperienceItem
             imgSrc="/app7.jpeg"
             imgAlt="app7"
@@ -73,8 +73,8 @@ export function Experience() {
             company="App7 Sistemas"
             period="fev de 2022 - ago de 2023 · 1 ano 7 meses"
             description={[
-              'Desenvolvimento de software whitelabel de iGaming, com foco em soluções estatísticas e customização de plataformas. Atuei no frontend utilizando AngularJS e React, criando interfaces dinâmicas e responsivas para proporcionar uma experiência de usuário eficiente e adaptável. Utilizei CSS e SASS para estilização, garantindo um design escalável e modular.',
-              'Trabalhei na implementação de componentes reutilizáveis e na integração com as equipes de backend e design para garantir a entrega de soluções personalizadas de alta performance e com ótima usabilidade em diversos dispositivos.',
+              'Desenvolvimento de software whitelabel de iGaming, com foco em soluções estatísticas e customização de plataformas...',
+              'Trabalhei na implementação de componentes reutilizáveis e na integração com as equipes...',
             ]}
           />
         </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,9 @@ export function Footer() {
       id="contact"
       className="bg-primary p-8 px-4 text-primary-foreground shadow-lg dark:bg-secondary dark:text-secondary-foreground md:px-40"
     >
-      <div className="container flex flex-col items-start px-96 text-left">
-        <h2 className="mb-4 max-w-3xl text-left text-lg">Sobre</h2>
-        <p className="mb-4 max-w-3xl text-left text-lg">
+      <div className="container flex flex-col items-start px-4 text-left md:px-20 lg:px-40">
+        <h2 className="mb-4 text-left text-2xl sm:text-3xl">Sobre</h2>
+        <p className="mb-4 max-w-3xl text-left text-base sm:text-lg md:text-xl">
           Desenvolvedor de Software com mais de 4 anos de experiência na entrega
           de soluções robustas e de alta qualidade. Especialista em React, React
           Native, Angular e Node.js, com sólida experiência na construção de
@@ -18,33 +18,30 @@ export function Footer() {
           TypeScript, além de ampla experiência em JavaScript, SQL e MongoDB.
         </p>
 
-        <div className="mb-4 flex flex-col items-center gap-6 sm:flex-row">
+        <div className="mb-4 flex flex-wrap items-center justify-center gap-6 sm:flex-row">
           <a
             href="https://github.com/stecks10"
             target="_blank"
             rel="noopener noreferrer"
+            className="transition hover:text-rose-400"
           >
-            <GithubLogo size={30} className="transition hover:text-rose-400" />
+            <GithubLogo size={30} />
           </a>
 
           <a
             href="mailto:vnn2006@gmail.com"
-            className="px-6 py-2 font-bold transition-colors dark:border-rose-600"
+            className="transition-colors hover:text-rose-400 dark:border-rose-600"
           >
-            <EnvelopeSimple
-              size={30}
-              className="transition hover:text-rose-400"
-            />
+            <EnvelopeSimple size={30} />
           </a>
+
           <a
             href="https://www.linkedin.com/in/vitor-nunes-do-nascimento-466004197/"
             target="_blank"
             rel="noopener noreferrer"
+            className="transition hover:text-rose-400"
           >
-            <LinkedinLogo
-              size={30}
-              className="transition hover:text-rose-400"
-            />
+            <LinkedinLogo size={30} />
           </a>
         </div>
       </div>
